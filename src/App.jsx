e43bbe0f2e08d83be0ddaa13fc7ca6b726e09013
Timothy Chia <timothycth9@gmail.com>
2023-09-19 01:01:03 +0800

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { A, B , C , D, E, G} from "./hooks"
-import "./index.css"; 
-
+import { A, B, C, D, E, F } from "./hooks";
+import "./index.css";
 
 function App() {
-  return(
-    <div className="custom-grid-container"> {/* Apply custom CSS class */}
-      <div className="grid-item"><A /></div>
-      <div className="grid-item"><B /></div>
-      <div className="grid-item"><C /></div>
-      <div className="grid-item"><D /></div>
-      <div className="grid-item"><E /></div>
-      <div className="grid-item"><G /></div>
+  return (
+
+    <>
+
+    <h1 className="dark-bg">How to React</h1>
+    <div className="custom-grid-container">
+      <div className="grid-item">
+        <A />
+      </div>
+      <div className="grid-item">
+        <B />
+      </div>
+      <div className="grid-item">
+        <C />
+      </div>
+      <div className="grid-item">
+        <D />
+      </div>
+      <div className="grid-item">
+        <E />
+      </div>
+      <div className="grid-item">
+        <F />
+      </div>
     </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/hooks.jsx
+++ src/hooks.jsx
@@ -1,20 +1,25 @@
-import { useState, useEffect,  useMemo, useContext, useReducer, useRef } from "react";
+import { useState, useEffect, useMemo, useReducer, useRef } from "react";
 import useBeforeUnloadAlert from "./custom";
-import "./main.css"
+import "./main.css";
+
+//UseState Hook
 
 export function A() {
-  // UseState Hook for handling count
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <>
       <h1 className="dark-bg">UseStateHook</h1>
-      <p className="dark-bg" > Count: {count}</p>
-      <button onClick={() => setCount(Math.max(count - 1, 0))}>-</button>
+      <p className="dark-bg"> Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>+</button>
-    </div>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </>
   );
 }
+
+
+
+//UseEffect Hook
 
 export function B() {
   const [data, setData] = useState(null);
@@ -56,117 +61,140 @@ export function B() {
   );
 }
 
-
-
+//UseMemo Hook
 
 export function C() {
-  // State to manage an input value
   const [inputValue, setInputValue] = useState("");
-  
-  // Function to calculate the square root of a number
-  const calculateSquareRoot = (number) => {
-    console.log(`Calculating square root of ${number}`);
-    return Math.sqrt(number);
-  };    
 
-  // Memoized calculation of square root
-  const memoizedSquareRoot = useMemo(() => calculateSquareRoot(inputValue), [inputValue]);
+  const calculateAverage = (numbers) => {
+    console.log('Calculating average...'); 
+  
+    const filteredNumbers = numbers
+      .filter((num) => num !== "")
+      .flatMap((num) => {
+        if (num.includes(',')) {
+          const parts = num.split(',');
+          return parts.map((part) => parseFloat(part) / 2);
+        } else {
+          return parseFloat(num);
+        }
+      });
+  
+    const sum = filteredNumbers.reduce((acc, num) => acc + num, 0);
+    return sum / filteredNumbers.length;
+  };
+
+  const numbersArray = inputValue.split(/[, ]+/);
+  const average = useMemo(() => calculateAverage(numbersArray), [numbersArray]);
 
   return (
     <div>
       <h1 className="dark-bg">UseMemo Hook</h1>
       <input
-        type="number"
+        className="input"
+        type="text"
         value={inputValue}
-        onChange={(e) => setInputValue(Number(e.target.value))}
+        onChange={(e) => setInputValue(e.target.value)}
       />
-      <p className="dark-bg">Square Root: {memoizedSquareRoot}</p>
+      <p className="dark-bg">Result: {isNaN(average) ? 'Please enter valid numbers' : average}</p>
     </div>
   );
 }
-
-
-
+//UseRef Hook
 
 export function D() {
-  // Ref to reference the input element
-  const inputRef = useRef(null);
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const intervalRef = useRef(null);
 
-  const focusInput = () => {
-    // Use the ref to focus on the input element
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  };
+  function handleStart() {
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+
+  function handleStop() {
+    clearInterval(intervalRef.current);
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
 
   return (
-    <div>
-      <h1 className="dark-bg">useRefHook</h1>
-      <input type="text" ref={inputRef} placeholder="Type something..." />
-      <button onClick={focusInput}>Focus Input</button>
-    </div>
+    <>
+      <h1 className="dark-bg">useRef Hook</h1>
+      <p className="dark-bg">Time passed: {secondsPassed.toFixed(3)}</p>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </>
   );
 }
 
+//Reducer Hook
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "incremented_age": {
+      return {
+        name: state.name,
+        age: state.age + 1,
+      };
+    }
+    case "changed_name": {
+      return {
+        name: action.nextName,
+        age: state.age,
+      };
+    }
+  }
+  throw Error("Unknown action: " + action.type);
+}
+const initialState = { name: "Taylor", age: 42 };
 
 export function E() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const counterReducer = (state, action) => {
-    switch (action.type) {
-      case "INCREMENT":
-        return { count: state.count + 1 };
-      case "DECREMENT":
-        return { count: state.count - 1 };
-      default:
-        return state;
-    }
-  };
-  
-  // Initialize state using useReducer
-  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
+  function handleButtonClick() {
+    dispatch({ type: "incremented_age" });
+  }
+
+  function handleInputChange(e) {
+    dispatch({
+      type: "changed_name",
+      nextName: e.target.value,
+    });
+  }
 
   return (
-    <div>
-      <h1 className="dark-bg">Counter using useReducer</h1>
-      <p className="dark-bg" > Count: {state.count}</p>
-      <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
-      <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</button>
-    </div>
+    <>
+      <h1 className="dark-bg"> UseReducerHook</h1>
+      <p className="dark-bg">
+        Hello, {state.name}. You are {state.age}.
+      </p>
+      <input
+        className="input"
+        value={state.name}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleButtonClick}>+</button>
+    </>
   );
 }
 
-export function G() {  
-  useBeforeUnloadAlert("You have unsaved changes. Are you sure you want to leave?");
+export function F() {
+  useBeforeUnloadAlert(
+    "You have unsaved changes. Are you sure you want to leave?"
+  );
 
   return (
     <div>
-      <h1 className="dark-bg">useBeforeUnloadAlert Hook</h1>
+      <h1 className="dark-bg">useCustom Hook</h1>
     </div>
   );
-}
-
-
-
-
-export async function AsyncFunction() {
-
-  //simulate API Call
-
-  
-
-
-
-
-  return (
-    <div>
-
-    </div>
-  )
-}
-
-
-export async function CallbackFunction() {
-
-  
 }
